9774fbf6c95bd5eea64d6847fa5781d18c1e8562
Hariram-Walia <hariramwalia@gmail.com>
2025-06-26 14:42:17 +0530

    Create status.c

---
+++ status.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main( int argc , string argv[])
+{
+    if (argc != 3 )
+{
+    printf("error \n ");
+    return 1;
+}
+printf("hello, %s\n", argv[1] );
+}
+// write echo $ ? in command
